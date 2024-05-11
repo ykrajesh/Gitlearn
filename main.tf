741115010b1f45e108447eef255a48bd16810dc0
YADAV <rajesh.c.yadav@capgemini.com>
2024-05-11 10:23:21 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.103.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
